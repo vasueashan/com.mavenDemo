7f20442aa7f3103caf49f5e5c4b4333b6d4abc6a
vasueashan <nariboina.vasu@gmail.com>
2021-09-16 14:52:38 +0530

    movied to git reporitory

---
+++ src/test/java/com/mavenDemo/login/loginTest.java
@@ -0,0 +1,17 @@
+package com.mavenDemo.login;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 login");
+	}
+	@Test
+	public void m2()
+	{
+		System.out.println("m2 login");
+	}
+}
---
+++ src/test/java/com/mavenDemo/order/orderTest.java
@@ -0,0 +1,18 @@
+package com.mavenDemo.order;
+
+import org.testng.annotations.Test;
+
+public class orderTest {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 order");
+	}
+	@Test
+	public void m2()
+	{
+		System.out.println("m2 order");
+	}
+
+}
