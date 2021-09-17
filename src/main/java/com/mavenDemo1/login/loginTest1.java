9fa9755b0a9dca3ac708d0251f3729b9df2cd2d9
vasueashan <nariboina.vasu@gmail.com>
2021-09-17 14:44:49 +0530

    added new package and java file

---
+++ src/main/java/com/mavenDemo1/login/loginTest1.java
@@ -0,0 +1,19 @@
+package com.mavenDemo1.login;
+
+
+
+public class loginTest1 {
+
+	
+	public void m1()
+	{
+		System.out.println("m1 login");
+		System.out.println("m1 login Edit3");
+		System.out.println("m1 loginvasuEdit6");//chnages  done
+	}
+
+	public void m2()
+	{
+		System.out.println("m2 login");
+	}
+}
