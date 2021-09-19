c14d4e7a2210f99ee8e778436a2d05f3eb15bc16
vasu <vsun21@gmail.com>
2021-09-19 11:03:00 +0530

    add files

--- src/test/java/com/mavenDemo/login/loginTest.java
+++ src/test/java/com/mavenDemo/login/loginTest.java
@@ -14,9 +14,22 @@ public class loginTest {
 		System.out.println("m1 login");
 		System.out.println("m1 login Edit3");
 		System.out.println("m1 loginvasuEdit6");//chnages  done
+		/*
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager/4.4.3");
+		*/
+		 // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\chromDriver\\chromedriver.exe");  
+          
+             // Instantiate a ChromeDriver class.     
+        WebDriver driver=new ChromeDriver();  
+          
+           // Launch Website  
+        driver.navigate().to("http://www.javatpoint.com/");  
+          
+         //Maximize the browser  
+          driver.manage().window().maximize();  
 		Thread.sleep(2000);
 		driver.quit();
 		System.out.println("Ended login");
