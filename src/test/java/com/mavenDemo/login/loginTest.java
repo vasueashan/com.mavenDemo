2b427c9b86a048b5e79c9998ed24a0094a827c5b
vasueashan <47999287+vasueashan@users.noreply.github.com>
2021-09-20 13:32:01 +0530

    Update loginTest.java

--- src/test/java/com/mavenDemo/login/loginTest.java
+++ src/test/java/com/mavenDemo/login/loginTest.java
@@ -40,7 +40,7 @@ public class loginTest {
 		Thread.sleep(2000);
 		driver.quit();
 		System.out.println("Ended login");
-		System.out.println("Ended login2");
+		System.out.println("Ended login3");
 		
 	}
 	@Test
